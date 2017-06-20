f55a716441c49870ad03bdf92cc0474e72979d8c
adennis96 <adennis96@users.noreply.github.com>
2017-06-20 16:36:30 -0400

    adding avatar display to storytelling

--- lili_storytelling/src/story_telling.py
+++ lili_storytelling/src/story_telling.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import time
+import std_msgs.msg
 import lili_audio.srv
 import lili_audio.msg
 import actionlib
@@ -8,15 +9,23 @@ import rospy
 
 class LILIAvatar:
     def __init__(self):
-        self.speech_act = actionlib.SimpleActionClient('speech', lili_audio.msg.TTSAction)
+        self.speech_act = actionlib.SimpleActionClient('speech',
+            lili_audio.msg.TTSAction)
+        self.speech_act.wait_for_server()
         rospy.wait_for_service('recognize_speech')
-        self.listen_serv = rospy.ServiceProxy('recognize_speech', lili_audio.srv.RecognizeSpeech)
+        self.listen_serv = rospy.ServiceProxy('recognize_speech',
+            lili_audio.srv.RecognizeSpeech)
+        self.display_pub = rospy.Publisher('display', std_msgs.msg.String,
+            queue_size=10)
 
     def speak(self, text, block=True):
+        if block:
+            self.display_pub.publish(std_msgs.msg.String('lili_talking.gif'))
         goal = lili_audio.msg.TTSGoal(text)
         self.speech_act.send_goal(goal)
         if block:
             self.speech_act.wait_for_result()
+            self.display_pub.publish(std_msgs.msg.String('lili_idle.gif'))
 
     def listen(self):
         resp = self.listen_serv(lili_audio.srv.RecognizeSpeechRequest())
