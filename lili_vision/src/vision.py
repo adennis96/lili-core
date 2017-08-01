ed4dda80544d9281c38f9abc48ad7d0a094a89d1
adennis96 <adennis96@users.noreply.github.com>
2017-08-01 18:06:36 -0400

    adding haar-cascade detection

--- lili_vision/src/vision.py
+++ lili_vision/src/vision.py
@@ -9,12 +9,18 @@ class VisionNode:
     def __init__(self):
         self.sub = rospy.Subscriber('image', sensor_msgs.msg.Image, self.vision_handler)
         self.bridge = cv_bridge.CvBridge()
+        self.cascade = cv2.CascadeClassifier(rospy.get_param('~cascade'))
 
     def vision_handler(self, data):
         try:
             img = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except cv_bridge.CvBridgeError as e:
             rospy.logerr(e)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
         cv2.imshow('image', img)
         cv2.waitKey(1)
